866b10cdfcb779dde6e4f8c428ee5fd8b8d80cfa
siva1029 <ummareddy.wd@gmail.com>
2024-08-28 14:14:12 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GenAI Project',
+    version='1.0.0',
+    description='A tool for creating interview questions',
+    author="Siva",
+    author_email="ummareddy.wd@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
