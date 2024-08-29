cd534f54eb2b88b4f2b3e77baafa4dc91d425dd4
siva1029 <ummareddy.wd@gmail.com>
2024-08-29 21:55:08 +0530

    updated

--- template.py
+++ template.py
@@ -1,10 +1,15 @@
+# Import necessary libraries/modules
 import os
 from pathlib import Path
 import logging
 
+# Configurating logging settings
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+
+# Logging welcome message
 #logging.info("Hello world!")
 
+# List of files/folders to be created 
 list_of_files = [
     "src/__init__.py",
     "src/helper.py",
@@ -16,12 +21,18 @@ list_of_files = [
     "app.py",
 ]
 
+# Iterate over the each file in the list
 for filepath in list_of_files:
+    # converting the filepath to a path object
     filepath = Path(filepath)
+    # Splitting the file path into directory and file name
     file_dir,file_name = os.path.split(filepath)
+    # Print the directory
     print(file_dir)
 
+# Check if the file's directory is not exist
     if file_dir!="":
+        # create directory if it doesn't exist
         os.makedirs(file_dir, exist_ok=True)    
         logging.info(f"Created directory: {file_dir}")
         #with open(filepath, "w"):
@@ -29,6 +40,7 @@ for filepath in list_of_files:
             # Write some content to the file for example purposes
             # with open(filepath, "w") as f:
             #     f.write("This is a sample content")
+    # Checking if the file doesn't exist or is empty
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
